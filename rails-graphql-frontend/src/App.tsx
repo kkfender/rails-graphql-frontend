a937d45dcd05036374d224419daf1a9591fc3232
kkfender <ibanez19851030@yahoo.co.jp>
2026-01-02 15:10:13 +0900

    Refactor App and Child components to use React.memo for performance optimization; add reset functionality in Child1 component.

--- rails-graphql-frontend/src/App.tsx
+++ rails-graphql-frontend/src/App.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, memo } from 'react'
 import { Child1 } from './app/components/Child1'
 import { Child4 } from './app/components/Child4'
 
-export const App = () => {
+export const App = memo(() => {
     console.log('Appレンダリング')
     const [num, setNum] = useState(0)
 
     const onClickButton = () => {
         setNum(num + 1)
     }
+    const onClickReset = () => {
+        setNum(0)
+    }
   return (
     <div>
         <button onClick={onClickButton}>ボタン</button>
         <p>現在の数値は{num}です</p>
-      <Child1 />
-      <Child4 />
+        <Child1 onClickReset={onClickReset}/>
+        <Child4 />
     </div>
   )
-}
+})
 
